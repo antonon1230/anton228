ccf041984f064bf815c3c8a0e5c708b7530eedff
itstart60 <marcevskijanton@gmai>
2023-12-02 10:00:36 +0200

    change code yakas fignya

---
+++ main.py
@@ -0,0 +1,14 @@
+from turtle import *
+import time
+shape("turtle")
+
+forward(100)
+time.sleep(1)
+right(90)
+forward(100)
+time.sleep(1)
+right(90)
+forward(100)
+time.sleep(1)
+right(90)
+forward(100)
